120793e846244f3e96907e84fc65947b76fea674
Mateusz Mozol <mozol.mateusz@gmail.com>
2025-05-04 22:15:07 +0200

    feat: add E2E test for creating meeting poll

---
+++ Pages/PollCreationPage.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+
+namespace RalllyTestingProject.Pages
+{
+    public class PollCreationPage : BasePage
+    {
+        private readonly By _titleInput = By.Name("title");
+        private readonly By _nextButton = By.CssSelector("button[type='submit']");
+        private readonly By _shareLink = By.CssSelector("span.min-w-0.truncate");
+
+        public PollCreationPage(IWebDriver driver) : base(driver) { }
+
+        public PollCreationPage WaitUntilLoaded()
+        {
+            WaitUntilVisible(_titleInput);
+            return this;
+        }
+
+        public PollCreationPage EnterTitle(string title)
+        {
+            var titleInput = WaitUntilVisible(_titleInput);
+            titleInput.Clear();
+            titleInput.SendKeys(title);
+            return this;
+        }
+
+        public PollCreationPage PickTodayDate()
+        {
+            int today = DateTime.Now.Day;
+
+            var dayButton = WaitUntilClickable(By.XPath($"//span[contains(@class, 'z-10') and text()='{today}']"));
+            dayButton.Click();
+
+            return this;
+        }
+
+        public PollCreationPage ClickNext()
+        {
+            WaitUntilClickable(_nextButton).Click();
+            return this;
+        }
+
+        public bool IsShareLinkDisplayed()
+        {
+            try
+            {
+                var linkSpan = WaitUntilVisible(_shareLink);
+                return linkSpan.Text.Contains("rallly.co/invite/");
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string GetPollLink()
+        {
+            var linkSpan = WaitUntilVisible(_shareLink);
+            return linkSpan.Text;
+        }
+    }
+}
---
+++ Tests/RalllyE2ETests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using RalllyTestingProject.Pages;
+
+namespace RalllyTestingProject.Tests
+{
+    [TestFixture]
+    public class RalllyE2ETests : BaseTest
+    {
+        [Test]
+        public void GivenUserCreatesPoll_WhenPollSubmitted_ThenPollLinkIsDisplayed()
+        {
+            HomePage.Open()
+                    .WaitUntilLoaded()
+                    .ClickCreatePoll();
+
+            var pollPage = GetPage<PollCreationPage>()
+                           .WaitUntilLoaded()
+                           .EnterTitle("My Automated Poll")
+                           .PickTodayDate()
+                           .ClickNext()
+                           .ClickNext(); // skip extra steps
+
+            Assert.That(pollPage.IsShareLinkDisplayed(), Is.True, "Poll link was not displayed.");
+        }
+
+        [Test]
+        public void GivenUserCreatesPoll_WhenPollLinkOpened_ThenPollIsDisplayed()
+        {
+            string testTitle = "My Second Automated Poll";
+            var pollPage = HomePage.Open()
+                                   .WaitUntilLoaded()
+                                   .ClickCreatePoll();
+
+            var creationPage = GetPage<PollCreationPage>()
+                               .WaitUntilLoaded()
+                               .EnterTitle(testTitle)
+                               .PickTodayDate()
+                               .ClickNext()
+                               .ClickNext();
+
+            string pollLink = creationPage.GetPollLink();
+
+            Driver.Navigate().GoToUrl("https://" + pollLink);
+
+            var pollPageObj = GetPage<PollPage>()
+                              .WaitUntilLoaded();
+
+            Assert.That(pollPageObj.IsAt(), Is.True, "Poll page was not loaded correctly.");
+            Assert.That(pollPageObj.GetPollTitle(), Is.EqualTo(testTitle), "Poll title mismatch.");
+        }
+    }
+}
